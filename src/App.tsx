c0975ca07fce4461833a6cbbe36ea5d2e263bdda
deviprasadkl <deviprasadofficial@gmail.com>
2025-01-08 11:05:44 +0530

    chore: add modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
+import { ModeToggle } from './components/mode-toggle'
 import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
-      <div className='flex flex-col gap-2 items-center justify-center h-screen'>
+      <div className='flex flex-col gap-4 items-center justify-center h-screen'>
         <h3 className='font-bold text-2xl'>Hello</h3>
         <Button>Button</Button>
+        <ModeToggle />
       </div>
     </>
   )
