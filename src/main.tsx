1fdb69c4277d94f5bdcad25a6fd706fba5ba9565
TheDarkLady <lakshmichaithanya13@gmail.com>
2025-07-07 16:51:24 +0530

    refactor: remove unused API key import and console log from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import { ThemeProvider } from "./components/ui/theme-provider.tsx";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "./firebase/AuthContext.tsx";
 
-const api = import.meta.env.VITE_API_KEY;
-console.log(api);
-
-
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <AuthProvider>
